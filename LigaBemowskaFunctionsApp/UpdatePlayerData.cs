41d77b4078bbf8b402f62e33bf0e9a95cbd588b8
Jakub Kalinowski <kalj@netcompany.com>
2023-09-18 21:47:31 +0200

    Added a GetAllPlayers Function

--- LigaBemowskaFunctionsApp/UpdatePlayerData.cs
+++ LigaBemowskaFunctionsApp/UpdatePlayerData.cs
@@ -5,18 +5,32 @@ using System.Linq;
 using HtmlAgilityPack;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Azure.Data.Tables;
+using Azure;
 
 namespace LigaBemowskaFunctionsApp
 {
     public class UpdatePlayerData
     {
         [FunctionName("UpdatePlayerData")]
-        public async Task RunAsync([TimerTrigger("*/5 * * * * *")]TimerInfo myTimer, ILogger log)
+        public async Task RunAsync([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
+
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=ligabemowskastats;AccountKey=8TMzEAfg6lzzV/VfBqZhCmzZMUwDcdYh81Laal2vqOrS7/q77gTlyccgxJcCCrFVQcAVxgEn214d+ASt+OZxkw==;EndpointSuffix=core.windows.net";
+            string tableName = "Players";
+
+            var serviceClient = new TableServiceClient(connectionString);
+            var tableClient = serviceClient.GetTableClient(tableName);
+
+            var id = 6302;
+
+
+
+
             using var httpClient = new HttpClient();
-            var html = await httpClient.GetStringAsync($"https://ligabemowska.pl/component/joomleague/player/1/1/6302");
+            var html = await httpClient.GetStringAsync($"https://ligabemowska.pl/component/joomleague/player/1/1/{id}");
 
             var document = new HtmlDocument();
             document.LoadHtml(html);
@@ -39,6 +53,43 @@ namespace LigaBemowskaFunctionsApp
             var redCards = statsCells.ElementAt(4).InnerHtml.Trim();
             var MOTMS = statsCells.ElementAt(5).InnerHtml.Trim();
 
+
+            var filter = TableClient.CreateQueryFilter($"RowKey eq '{id}'");
+
+            var queryResults = tableClient.Query<TableEntity>(filter);
+
+            var playerRecord = queryResults.SingleOrDefault();
+
+            if (playerRecord != null)
+            {
+                playerRecord["Name"] = name;
+                playerRecord["Appearances"] = appearances;
+                playerRecord["Goals"] = goals;
+                playerRecord["Assists"] = assists;
+                playerRecord["YellowCards"] = yellowCards;
+                playerRecord["RedCards"] = redCards;
+                playerRecord["MOTMS"] = MOTMS;
+
+                await tableClient.UpdateEntityAsync(playerRecord, ETag.All);
+                Console.WriteLine($"Updated Player #{id} successfully.");
+            }
+            else
+            {
+                playerRecord = new TableEntity("1", id.ToString())  // Partition is always 1 and RowKey is Player's Id
+                {
+                    { "Name", name },
+                    { "Appearances", appearances },
+                    { "Goals", goals },
+                    { "Assists", assists },
+                    { "YellowCards", yellowCards },
+                    { "RedCards", redCards },
+                    { "MOTMS", MOTMS }
+                };
+
+                await tableClient.AddEntityAsync(playerRecord);
+
+                Console.WriteLine($"Inserted a new Player #{id} successfully.");
+            }
         }
     }
 }
