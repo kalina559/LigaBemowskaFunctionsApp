61689af2e2b3c6f19f761310d2e4da14bbce598a
kalj <kalj@netcompany.com>
2023-09-18 20:19:20 +0200

    Add project files.

---
+++ LigaBemowskaFunctionsApp/UpdatePlayerData.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Net;
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LigaBemowskaFunctionsApp
+{
+    public class Function1
+    {
+        [FunctionName("UpdatePlayerData")]
+        public async Task RunAsync([TimerTrigger("*/5 * * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            using var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync($"https://ligabemowska.pl/component/joomleague/player/1/1/6302");
+
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
+
+
+            var playerNameDiv = document.DocumentNode.SelectSingleNode("//div[@class='player-desc m-4 player-text']");
+
+            var playerName = playerNameDiv.Descendants().Where(n => n.Name == "h3").SingleOrDefault();
+            var name = playerName.InnerHtml.Trim();
+
+            var stats_total = document.DocumentNode.SelectSingleNode("//tr[@class='career_stats_total']");
+
+            //0 - appearances, 1 - goals, 2 - assists, 3 - yellow cards, 4 - red cards, 5 - motm
+            var statsCells = stats_total.Descendants(0).Where(n => n.HasClass("td_c"));
+
+            var appearances = statsCells.ElementAt(0).InnerHtml.Trim();
+            var goals = statsCells.ElementAt(1).InnerHtml.Trim();
+            var assists = statsCells.ElementAt(2).InnerHtml.Trim();
+            var yellowCards = statsCells.ElementAt(3).InnerHtml.Trim();
+            var redCards = statsCells.ElementAt(4).InnerHtml.Trim();
+            var MOTMS = statsCells.ElementAt(5).InnerHtml.Trim();
+
+        }
+    }
+}
