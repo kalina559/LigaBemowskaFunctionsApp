c6752609e9e571095fa34969dfb5a85a145ec543
Jakub Kalinowski <kalj@netcompany.com>
2023-09-18 22:48:55 +0200

    GetAllPlayers is now anonymous

--- LigaBemowskaFunctionsApp/GetAllPlayers.cs
+++ LigaBemowskaFunctionsApp/GetAllPlayers.cs
@@ -17,7 +17,7 @@ namespace LigaBemowskaFunctionsApp
     {
         [FunctionName("GetAllPlayers")]
         public static string Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
