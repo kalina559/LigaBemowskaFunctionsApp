2ddee8073f02c0a3be9373e4e9c09fc6b358cdf7
Jakub Kalinowski <kalj@netcompany.com>
2023-09-19 18:52:07 +0200

    Checking performance running in azure

--- LigaBemowskaFunctionsApp/Functions/UpdatePlayerData.cs
+++ LigaBemowskaFunctionsApp/Functions/UpdatePlayerData.cs
@@ -9,29 +9,41 @@ namespace LigaBemowskaFunctionsApp.Functions
     public class UpdatePlayerData
     {
         [FunctionName("UpdatePlayerData")]
-        public async Task RunAsync([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer, ILogger log)
+        public async Task RunAsync([TimerTrigger("0 19 * * TUE")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"Started UpdatePlayerData function executed at: {DateTime.Now}");
 
             var playerService = new PlayerService();
 
-            var counter = 0;
-            var id = 0;
+            //var counter = 0;
+            //var id = 0;
 
-            while (counter < 20)
+            //while (counter < 20)
+            //{
+            //    try
+            //    {
+            //        counter--;
+            //        await playerService.AddOrUpdatePlayer(id);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        counter++;
+            //        log.LogError($"Failed to upload Player %{id} because of {ex.Message}");
+            //    }
+
+            //    id++;
+            //}
+
+            for (int i = 100; i < 600; i++)
             {
                 try
                 {
-                    counter--;
-                    await playerService.AddOrUpdatePlayer(id);
+                    await playerService.AddOrUpdatePlayer(i);
                 }
                 catch (Exception ex)
                 {
-                    counter++;
                     log.LogError($"Failed to upload Player %{i} because of {ex.Message}");
                 }
-
-                id++;
             }
 
             log.LogInformation($"Finished UpdatePlayerData function executed at: {DateTime.Now}");
